7c879021b7975056393f1ba08d3b0c3f5e78f5af
Jed Dylan Lee <leedylan6058@gmail.com>
2021-03-30 16:44:31 +0800

    Changes in sidebar

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <Sidebar/>
+  <div class="flex min-h-screen">
+    <div class="w-64 flex-shrink-0">
+      <Sidebar/>
+    </div>
+    <div class="flex flex-col w-full">
+      <header class="text-white bg-blue-700 shadow-lg sticky left-auto top-0 right-0">
+        <div class="h-14 px-6 flex relative items-center justify-between">
+          <div class="flex items-center">
+            <img src="../../assets/logo.png" alt="" class="h-10 w-10 mr-3 object-contain">
+            <h1 class="text-lg font-bold font-sans tracking-wide uppercase">PGSO Inventory</h1>
+          </div>
+          <div>
+            <ul class="flex flex-row">
+              <li>Home</li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </div>
   </div>
 </template>
 
@@ -12,6 +29,7 @@ export default {
   components: {
     Sidebar
   },
+
   computed: mapState(["userData"]),
   created(){
     console.log(this.userData)
