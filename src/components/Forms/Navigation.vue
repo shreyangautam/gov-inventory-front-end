69b746bd745a50a61481a670703c16905e2024b3
Jed Dylan Lee <leedylan6058@gmail.com>
2021-03-30 23:24:28 +0800

    Added logout button

--- src/components/Forms/Navigation.vue
+++ src/components/Forms/Navigation.vue
@@ -6,13 +6,19 @@
             <p class="text-white text-md font-semibold tracking-wider uppercase">Dashboard</p>
           </div>
           <div>
-            <div class="flex flex-row space-x-2">
+            <div class="flex flex-row space-x-2 items-center">
               <span>
-                <a href=""><img src="../../assets/svg/bell.svg" alt="icon" class="h-4 w-4"></a>
+                <a href="#"><img src="../../assets/svg/bell.svg" alt="icon" class="h-4 w-4"></a>
               </span>
-              <span>
-                <a href=""><img src="../../assets/svg/settings.svg" alt="icon" class="h-4 w-4"></a>
+              <span class="border-r border-gray-200 pr-4">
+                <a href="#"><img src="../../assets/svg/settings.svg" alt="icon" class="h-4 w-4"></a>
               </span>
+              <div class="flex flex-row items-center">
+                  <span class="ml-2">
+                      <img src="../../assets/svg/log-out.svg" alt="icon-logout" class="h-4 w-4 mr-1">
+                  </span>
+                  <a href="#" class="hover:underline">Logout</a>
+              </div>
             </div>
           </div>
         </div>
