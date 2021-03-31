43e5c43d9c126b3aa54e3d035bebcb92122f5051
Jed Dylan Lee <leedylan6058@gmail.com>
2021-03-31 17:26:07 +0800

    final changes in sidebar and navigation

--- src/components/Forms/Navigation.vue
+++ src/components/Forms/Navigation.vue
@@ -1,23 +1,31 @@
 <template>
-    <div class="flex flex-col w-full">
-      <header class="text-white bg-blue-500 shadow-lg sticky left-auto top-0 right-0">
-        <div class="h-12 px-6 flex relative items-center justify-between">
-          <div class="flex items-center">
-            <p class="text-white text-md font-semibold tracking-wider uppercase">Dashboard</p>
+    <div :class="containerBar">
+      <header :class="navigationBar">
+        <div :class="navigationBox">
+          <div class="flex">
+            <input
+              type="search"
+              id="search"
+              placeholder="Search"
+              :class="searchBar"
+              v-model="searchBox"
+            />
           </div>
           <div>
-            <div class="flex flex-row space-x-2 items-center">
+            <div :class="iconsBox">
               <span>
                 <a href="#"><img src="../../assets/svg/bell.svg" alt="icon" class="h-4 w-4"></a>
               </span>
               <span class="border-r border-gray-200 pr-4">
                 <a href="#"><img src="../../assets/svg/settings.svg" alt="icon" class="h-4 w-4"></a>
               </span>
-              <div class="flex flex-row items-center">
-                  <span class="ml-2">
-                      <img src="../../assets/svg/log-out.svg" alt="icon-logout" class="h-4 w-4 mr-1">
-                  </span>
-                  <a href="#" class="hover:underline">Logout</a>
+              <div :class="logoutBox">
+                  <a href="#" class="hover:underline flex items-center">
+                    <span class="ml-2">
+                        <img src="../../assets/svg/log-out.svg" alt="icon-logout" class="h-4 w-4 mr-1">
+                    </span>
+                    Logout
+                  </a>
               </div>
             </div>
           </div>
@@ -28,6 +36,15 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            containerBar: "flex flex-col w-full",
+            navigationBar: "text-white bg-blue-500 shadow-lg sticky left-auto top-0 right-0",
+            navigationBox: "h-12 px-6 flex relative items-center justify-between",
+            searchBar: "px-4 transition duration-100 text-black border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-400",
+            iconsBox: "flex flex-row space-x-2 items-center",
+            logoutBox: "flex flex-row items-center",
+        }
+    }
 }
 </script>
