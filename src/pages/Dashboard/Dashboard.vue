cf80993085c3dc9ea8b7f72bf6c5422568622428
jeph-kun <jephv4@gmail.com>
2021-04-03 22:36:17 +0800

    Sidebar fixes

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex min-h-screen">
-    <div class="w-60 flex-shrink-0">
+    <div class="w-64 flex-shrink-0">
       <Sidebar :modules="modules"/>
     </div>
-    <div class=" relative flex flex-col w-full">
-      <Navigation/>
-      <h1 class="mt-6 ml-6 font-bold uppercase tracking-wide">Dashboard</h1>
-      <router-view></router-view>
+    <div class="relative flex flex-col w-full bg-primary pr-8 py-5">
+      <div class="relative flex flex-col w-full bg-gray-100 rounded-3xl h-screen">
+          <!-- BRAD DIDI MO IBUTANG AN COMPONENT SAN NAVIGATION -->
+          <h1 class="mt-6 ml-6 font-bold uppercase tracking-wide">Dashboard</h1>
+           <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
