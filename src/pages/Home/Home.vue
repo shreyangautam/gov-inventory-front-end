2d2a3cbfa57c7f464ffe6dedf79739487f1614d1
Jed Dylan Lee <leedylan6058@gmail.com>
2021-04-16 13:23:28 +0800

    Adjustment Home Placeholder

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col items-center w-full h-full py-20 px-90">
-       <img src="../../assets/welcome.svg" width="450"/>
-       <h1 class="text-4xl pt-10 text-gray-500 font-semibold">Welcome User!</h1>
+       <img src="../../assets/welcome.svg" width="450" class="opacity-30"/>
+       <h1 class="text-4xl pt-10 text-gray-300 font-semibold">Welcome User!</h1>
     </div>
 </template>
 
