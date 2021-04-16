02736e62435fb50f1a3aeda59a6d0a31c1e8a985
Jed Dylan Lee <leedylan6058@gmail.com>
2021-04-16 15:31:26 +0800

    Same changes

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center w-full h-full py-20 px-90">
-       <img src="../../assets/welcome.svg" width="450" class="opacity-30"/>
+       <img src="../../assets/welcome.svg" width="450" class="opacity-20"/>
        <h1 class="text-4xl pt-10 text-gray-300 font-semibold">Welcome User!</h1>
     </div>
 </template>
